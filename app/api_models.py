4e28ca2031a05364c42ca830a0ba5a3c8459cd81
Prashant-Mamdyal <prashant.mamdyal68@gmail.com>
2024-07-07 18:47:28 +0530

    created order endpoints

--- app/api_models.py
+++ app/api_models.py
@@ -1,10 +1,47 @@
 from flask_restx import fields
 from .extensions import api
 
+product_model = api.model("Product", {
+    "id": fields.Integer,
+    "name": fields.String,
+    "description": fields.String,
+    "price": fields.Float,
+    "stock": fields.Integer
+})
+
+
+orderitem_model = api.model("OrderItem", {
+    "orderitem_id": fields.Integer,
+    "product_id":fields.Nested(product_model),
+    "quantity": fields.Integer,
+    "price": fields.Float
+})
+
+
+order_model = api.model("Order", {
+    "id": fields.Integer,
+    "customer_id":fields.Integer,
+    "order_date": fields.DateTime,
+    "status": fields.String,
+    "order_items":fields.Nested(orderitem_model)
+})
+
+order_input_model = api.model("Order", {
+    "customer_id":fields.Integer,
+    "order_date":fields.DateTime,
+    "status": fields.String
+})
+
+order_input_status_model = api.model("Order", {
+    "status":fields.String
+})
+
+
 customer_model = api.model("Customer",{
     "id" : fields.Integer,
     "name": fields.String,
-    "contact_info" : fields.String
+    "contact_info" : fields.String,
+    "orders": fields.Nested(order_model)
 })
 
 customer_input_model = api.model("Customer",{
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import request
 from flask_restx import Resource, Namespace
-from .models import Customer
-from .api_models import customer_model, customer_input_model
+from .models import Customer, Order, OrderItem
+from .api_models import customer_model, customer_input_model, order_model, order_input_model, order_input_status_model, orderitem_model
 from .extensions import db
 
 customers_ns = Namespace("customers", description='Customers endpoints')
@@ -47,3 +47,62 @@ class CustomerDetails(Resource):
         db.session.delete(customer) 
         db.session.commit()
         return customer, 200
+    
+#-----------------------------------------------
+@orders_ns.route("/")
+class OrderList(Resource):
+    @orders_ns.marshal_list_with(order_model)
+    def get(self):
+        order = Order.query.all()
+        return order, 200
+
+    @orders_ns.expect(order_input_model)
+    @orders_ns.marshal_with(order_model)
+    def post(self):
+        data = request.json
+        new_order = Order(customer_id = data['customer_id'], order_date = data['order_date'], status = data['status'])
+        db.session.add(new_order)
+        db.session.commit()
+        return new_order , 201
+    
+@orders_ns.route("/<int:order_id>")
+class OrderDetails(Resource):
+    @orders_ns.marshal_with(order_model)
+    def get(self, order_id):
+        new_order = Order.query.get_or_404(order_id)
+        return new_order, 200
+
+    @orders_ns.expect(order_input_model)
+    @orders_ns.marshal_with(order_model)
+    def put(self, order_id):
+        data = request.json
+        order = Order.query.get(order_id)
+        order.customer_id = data['customer_id']
+        order.order_date = data['order_date']
+        order.status = data['status']
+        db.session.commit()
+        return order, 200 
+    
+    def delete(self, order_id):
+        order = Order.query.get_or_404(order_id)
+        db.session.delete(order)
+        db.session.commit()
+        return {"message":"Order deleted successfully"}, 200
+    
+@orders_ns.route("/<int:order_id>/status")
+class OrderStatus(Resource):
+    @orders_ns.expect(order_input_status_model)
+    @orders_ns.marshal_with(order_model)
+    def patch(self, order_id):
+        data = request.json
+        order = Order.query.get_or_404(order_id)
+        order.status = data['status']
+        db.session.commit()
+        return order, 200
+    
+@orders_ns.route("<int:order_id>/items")
+class OrderItemsList(Resource):
+    @orders_ns.marshal_list_with(orderitem_model)
+    def get(self, order_id):
+        orderitem = OrderItem.query.get(order_id)
+        return orderitem, 200
