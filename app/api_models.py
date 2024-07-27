78318219f147ad1bca9b27029d66423dab920114
Prashant-Mamdyal <prashant.mamdyal68@gmail.com>
2024-07-27 13:51:09 +0530

    updated models and api_model files

--- app/api_models.py
+++ app/api_models.py
@@ -9,42 +9,40 @@ product_model = api.model("Product", {
     "stock": fields.Integer
 })
 
-
 orderitem_model = api.model("OrderItem", {
-    "orderitem_id": fields.Integer,
-    "product_id":fields.Nested(product_model),
+    "id": fields.Integer,
+    "product": fields.Nested(product_model),
     "quantity": fields.Integer,
     "price": fields.Float
 })
 
-
 order_model = api.model("Order", {
     "id": fields.Integer,
-    "customer_id":fields.Integer,
+    "customer_id": fields.Integer,
     "order_date": fields.DateTime,
     "status": fields.String,
-    "order_items":fields.Nested(orderitem_model)
+    #"order_items":fields.Nested(orderitem_model)
 })
 
 order_input_model = api.model("Order", {
-    "customer_id":fields.Integer,
-    "order_date":fields.DateTime,
+    "customer_id": fields.Integer,
+    "order_date": fields.DateTime,
     "status": fields.String
 })
 
-order_input_status_model = api.model("Order", {
-    "status":fields.String
+order_input_status_model = api.model("OrderStatus", {
+    "status": fields.String
 })
-
 
 customer_model = api.model("Customer",{
     "id" : fields.Integer,
     "name": fields.String,
     "contact_info" : fields.String,
-    "orders": fields.Nested(order_model)
+    # "orders": fields.Nested(order_model)
 })
 
 customer_input_model = api.model("Customer",{
     "name": fields.String,
     "contact_info": fields.String
 })
+
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from datetime import datetime
 from .extensions import db
+from datetime import datetime
+
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
