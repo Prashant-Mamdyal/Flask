69db3e458aef6953c5c5a226ed0af6ab49257fc8
Prashant-Mamdyal <prashant.mamdyal68@gmail.com>
2024-07-27 13:54:48 +0530

    configuring testing env in create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,16 @@ from flask import Flask
 from .extensions import api, db
 from .routes import customers_ns, orders_ns
 
-def create_app():
+def create_app(config_name = None):
     app = Flask(__name__)
 
-    #db.config['SQLALCHEMY_DATABASE_URI']='mysql://username:password@localhost/db_name'
-    app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:1234@localhost/flask_app'
+    if config_name == 'testing':
+        app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///:memory:'
+        app.config['Testing']= True
+    else:
+        #db.config['SQLALCHEMY_DATABASE_URI']='mysql://username:password@localhost/db_name'
+        app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:1234@localhost/flask_app'
+        
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
 
